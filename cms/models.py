3a81ce50c5ad883dd5c112413df11944b84a5b81
VitorSDDF <vitor.sd@hotmail.com>
2017-11-16 00:14:56 -0200

    Classe Annotator

--- cms/models.py
+++ cms/models.py
@@ -4,6 +4,87 @@ import MySQLdb
 
 # Create your models here.
 
+class Anottator(models.Model):
+
+    def get_article_concepts(concept_text_list,text):#retorna lista de conceitos encontrados no texto
+    
+        sentences = get_text_sentences(text)
+        concepts_found = list()
+        search_remaining_concepts = [nltk.word_tokenize(i) for i in concept_text_list]
+
+        while len(search_remaining_concepts) > 0:
+            biggest_concept_lenght = len(max(search_remaining_concepts,key = len))
+            biggest_concept_items = [x for x in search_remaining_concepts if len(x) == biggest_concept_lenght]
+            search_remaining_concepts = [x for x in search_remaining_concepts if x not in biggest_concept_items]
+                
+            for sentence in sentences:
+                if len(sentence) >= biggest_concept_lenght:
+                    for k in range(0,len(sentence) - biggest_concept_lenght):
+                        biggest_concept_items_cp = biggest_concept_items
+                        for concept in biggest_concept_items_cp:
+                            if concept not in concepts_found:
+                            #Compara o conceito com os itens  de -(k + biggest_concept_lenght) até -k
+                                if [i.upper() for i in concept] == [i.upper() for i in sentence[-(k + biggest_concept_lenght):-k]]:
+                                    concepts_found.insert(len(concepts_found),concept)
+                                    biggest_concept_items.remove(concept)
+
+        return concepts_found
+
+    def get_reifications(onto):#retorna a lista de uris de todas as reificações de uma ontologia carregada no ontospy
+        reifications = []
+        for i in onto.classes:
+            if not i.children():
+                reifications.insert(len(reifications),i)
+        return reifications
+
+    def add_related_concepts(parents,elem_set):#Adiciona a um conjunto de uris(nós) todos os outros nós pais relacionádos aos mesmos
+        for i in parents:
+            elem_set.update([i])
+            add_related_concepts(i.parents(),elem_set)
+
+    def get_text_sentences(text):
+        sentences = nltk.sent_tokenize(' '.join([i for i in text.split()]))
+        return [nltk.word_tokenize(sent) for sent in sentences]
+
+    def update_graph(basefile,doc_ref,annotations_concepts_uris,author):#Insere novas anotações no grafo presente em basefile ou cria um novo
+    
+        AO = Namespace("http://smiy.sourceforge.net/ao/rdf/associationontology.owl")
+        PAV = Namespace("http://cdn.rawgit.com/pav-ontology/pav/2.0/pav.owl")
+        #ANN = Namespace("https://www.w3.org/2000/10/annotation-ns#annotates")
+        AOF = Namespace("http://annotation-ontology.googlecode.com/svn/trunk/annotation-foaf.owl")
+
+        graph = Graph()
+        if os.path.isfile(basefile):
+            graph.parse(basefile, format="xml")
+        else:
+            print("Arquivo de anotações não encontrado ou ilegível.Outro será criado")
+            graph.bind('aof',AOF)
+            graph.bind('ao',AO)
+            graph.bind('pav',PAV)
+
+            graph.commit()
+
+        for i in annotations_concepts_uris:
+            d = Describer(graph)        
+            d.rel(RDF.type,AO.Annotation)
+            d.rel(AOF.annotatesDocument,doc_ref)
+            d.rel(AO.hasTopic,i.uri)
+            d.rel(PAV.createdOn,Literal(datetime.datetime.now(),datatype=XSD.date))
+            d.rel(PAV.createdB,Literal(author))
+            graph.commit()
+
+                                    
+        return graph
+
+    def uri_to_text(uri_to_text_func,concepts):
+        concept_dict = dict()
+        for i in concepts:
+            concept_dict[uri_to_text_func(i)] = i
+        return concept_dict
+
+    def zika_ontology_uri_to_text:
+        return str(uri).partition('#')[-1].replace('_',' ')
+
 class Creator(models.Model):
     name = models.CharField(max_length=50)
     email = models.URLField()
@@ -38,6 +119,19 @@ class Artigo(models.Model):
         return texto
 
     def save(self, *args, **kwargs):
+
+        '''onto = ontospy.Ontospy("root-ontology.owl")
+        a = Annotator()
+        web_concepts = a.get_reifications(onto)
+         
+        concepts_dict = a.uri_to_text(a.zika_ontology_uri_to_text,web_concepts)
+
+        reifications_to_annotate = [concpts_dict[i] for i in a.get_article_concepts(concept_dict.keys(),text)]
+        concepts_to_annotate = set()
+        a.add_related_concepts(reifications_to_annotate,concepts_to_annotate)
+
+        a.update_graph("base.rdf",url,list(concepts_to_annotate),"Vitor Silva").serialize(format='xml',destination = 'base.rdf')'''
+
         chave = self.title.split()
         texto = ""
         for i in chave:
